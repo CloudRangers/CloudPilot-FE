e1f43c2063ccc64bc7d6d5f6151fcf7b4d32b722
2jeong2 <jeongyi0131@naver.com>
2025-11-28 09:39:52 +0900

    [refactor]#28 - next.config.mjs rewrites 코드 추가

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/backend/:path*",
+        destination: "http://localhost:8080/:path*",
+      },
+    ];
+  }
+
   // 🔹 rewrites는 S3 정적 호스팅에서는 동작하지 않으니 제거
 }
 
